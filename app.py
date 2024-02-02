1711510db142148373c018b2df554d5837e3eccd
kunal0301 <kunalryan03@gmail.com>
2024-02-02 17:10:59 +0530

    Completed

--- app.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from os import getenv
-# this file is used to run project on versel
-if __name__ == "__main__":
-    port = int (getenv("PORT",8000))
-    uvicorn.run("main:app",host="0.0.0.0",port=port,reload=True)
